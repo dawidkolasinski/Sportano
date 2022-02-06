97e5dfdf08f5ab7b555246d4a4eb6a7af11edb63
dawidkolasinski <dawid.m.kolasinski@gmail.com>
2022-02-06 09:18:12 +0100

    Initial Commit

---
+++ Sportano/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sportano
+//
+//  Created by Dawid Kolasinski on 06/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sportano/SportanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportanoApp.swift
+//  Sportano
+//
+//  Created by Dawid Kolasinski on 06/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SportanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
